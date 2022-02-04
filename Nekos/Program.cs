beb06455bf5a6d3772852d173d83e05ed7770381
MarkenJaden <jjsch1410@gmail.com>
2022-02-04 10:07:37 +0100

    Added keyboard navigation gif

--- Nekos/Program.cs
+++ Nekos/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
