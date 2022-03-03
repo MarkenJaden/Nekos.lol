26e168bd72f3cdbb4e5b4bbe2c361a53afd00723
MarkenJaden <jjsch1410@gmail.com>
2022-03-03 18:57:06 +0100

    Added copy link to clipboard button

--- Nekos/Program.cs
+++ Nekos/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using CurrieTechnologies.Razor.Clipboard;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddClipboard();
 
 var app = builder.Build();
 
